a1b73d2286a9a4624a86feae6d21149cfa2b8f79
Kavya Kodicharla <kavyakodicharla9920@gmail.com>
2026-02-17 20:36:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme:{
+        gridTemplateColumns:{
+            'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        fontFamily:{
+            outfit: ["Outfit", "sans-serif"],
+            ovo: ["Ovo", "serif"]
+        },
+        
+        }
+    }
